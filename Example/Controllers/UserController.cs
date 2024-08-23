a21dd41726265619de75fd4f1a7fd848df2941b0
smile <smile@DESKTOP-9LLAKHH>
2024-08-23 19:39:06 +0300

    Add project files.

---
+++ Example/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Name = "Oleg" },
+                new { Name = "Ivan" }
+            };
+            return Ok(users);
+        }
+    }
+}
